69000a026e7097a3ce88c68b09582552cecee3e2
root <root@localhost.(none)>
2016-09-27 22:23:25 +0800

    big change

--- game5.bak.py
+++ game5.bak.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python
 #-*-coding:utf-8-*-
-#增加了random函数,优化UI
+#4.0增加了random函数,优化UI
+#5.0新增输入检测机制,开始采用isinstance，后来采用for in循环
 import random
 answer = random.randint(1,9) 
 print('\n****游戏程序版本四****')
 print('Build By Jason\n')
 i=3
+c=2
 temp = input('请输入一个个位数: ')
+while not  isinstance(temp,int) :
+	print('请检查你的输入！')
+	temp = input('请输入一个个位数: ')
+	c-=1
+	if c==0:
+		print('请稍后再试，程序已退出！')
+		exit()
+	else :
+		continue
 guess = int(temp)
 while  i>0 :
 	if guess == answer :
@@ -17,7 +28,7 @@ while  i>0 :
 			print('整大了')
 		else:
 			print('整小了')
-	i=i-1 
+	i-=1 
 	if i != 0 : 
 		temp = input('请输入一个个位数: ')
 		guess = int(temp)
